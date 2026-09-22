f5087b84b27fb0752061830c8c84bac439f93847
FredyNunez05 <ncfrdy005@gmail.com>
2026-09-21 20:47:07 -0500

    Se agregó el objeto Scanner

--- src/consola/instituto/ConsolaInstituto.java
+++ src/consola/instituto/ConsolaInstituto.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package consola.instituto;
-
+import java.util.Scanner;
 /**
  *
  * @author UCA40411
@@ -15,6 +15,9 @@ public class ConsolaInstituto {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+         Scanner scanner = new Scanner(System.in);
+         AlumnoController controlador = new AlumnoController();
+         boolean siguiente = true;
     }
     
 }
